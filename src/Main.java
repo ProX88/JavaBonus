9340395e89e0e1e694f3b1a3f5b6a4ecf154dac1
ParadoX <djei007@mail.ru>
2023-07-05 18:51:57 +0300

    JavaBonus

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balanc = 100500;
+        int payment = 3000;
+        int bonus ;
+        if (payment < 1000)
+            bonus=0;
+        else
+            bonus = payment / 100;
+        int itog = balanc + payment + bonus;
+        System.out.println(itog);
+        System.out.println(bonus);
+    }
+}
